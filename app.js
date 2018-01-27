9503ecab0098652c0d94b2369d47853184e97e0a
CheshireWho <brodbeck.birgit@gmail.com>
2018-01-27 14:31:37 +0100

    Get pins count for rolls

--- app.js
+++ app.js
@@ -7,11 +7,46 @@
 
 	// I chose the module pattern to keep implementation details private.
 	function Game() {
-		let count = 0;
+		const maxFrames = 10;
+		const maxRolls = 2;
+		const minRoll = 1;
+		const maxPinsPerFrame = 10;
+		const minPinsPerFrame = 0;
+
+		let currentFrame = 1;
+		let currentRoll = 1;
+		let hitPinsInFrame = minPinsPerFrame;
+		let hitPinsPerRoll = minPinsPerFrame;
 
 		function play() {
-			count++;
-			console.log('playing ' + count);
+			// roll ball
+			hitPinsPerRoll = getRandomIntInclusive(minPinsPerFrame, maxPinsPerFrame - hitPinsInFrame);
+			hitPinsInFrame += hitPinsPerRoll;
+
+			// update scoring table
+			document.getElementById(`frame-${currentFrame}-roll-${currentRoll}`).textContent = hitPinsPerRoll;
+
+			currentRoll++;
+			if (currentRoll > maxRolls) {
+				currentFrame++;
+				currentRoll = minRoll;
+				hitPinsInFrame = minPinsPerFrame;
+			}
+
+			checkTerminationCondition();
+		}
+
+		// copied from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+		function getRandomIntInclusive(min, max) {
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min + 1)) + min; 
+		}
+
+		function checkTerminationCondition() {
+			if (currentFrame > maxFrames) {
+				button.disabled = true;
+			}
 		}
 
 		return {
