8145180343a577695f68363e46b2d9e17958dd7d
CheshireWho <brodbeck.birgit@gmail.com>
2018-01-27 14:51:19 +0100

    Apply bowling symbols and strike rules for hit pins per roll

--- app.js
+++ app.js
@@ -17,21 +17,12 @@
 		let currentRoll = 1;
 		let hitPinsInFrame = minPinsPerFrame;
 		let hitPinsPerRoll = minPinsPerFrame;
+		let hitPinsVisual;
 
 		function play() {
-			// roll ball
-			hitPinsPerRoll = getRandomIntInclusive(minPinsPerFrame, maxPinsPerFrame - hitPinsInFrame);
-			hitPinsInFrame += hitPinsPerRoll;
+			roll();
 
-			// update scoring table
-			document.getElementById(`frame-${currentFrame}-roll-${currentRoll}`).textContent = hitPinsPerRoll;
-
-			currentRoll++;
-			if (currentRoll > maxRolls) {
-				currentFrame++;
-				currentRoll = minRoll;
-				hitPinsInFrame = minPinsPerFrame;
-			}
+			handleFrameStatus();
 
 			checkTerminationCondition();
 		}
@@ -41,6 +32,52 @@
 			min = Math.ceil(min);
 			max = Math.floor(max);
 			return Math.floor(Math.random() * (max - min + 1)) + min; 
+		}
+
+		function roll() {
+			let isStrike = false;
+
+			hitPinsPerRoll = getRandomIntInclusive(minPinsPerFrame, maxPinsPerFrame - hitPinsInFrame);
+			hitPinsInFrame += hitPinsPerRoll;
+
+			// determine symbol to show in scoring table
+			switch (true) {
+				case hitPinsPerRoll === minPinsPerFrame:
+					hitPinsVisual = '-';
+					break;
+				case hitPinsInFrame === maxPinsPerFrame:
+					if (currentRoll === minRoll) {
+						hitPinsVisual = 'X';
+						isStrike = true;
+					} else {
+						hitPinsVisual = '/';
+					}
+					
+					break;
+				default:
+					hitPinsVisual = hitPinsPerRoll;
+			}
+
+			// update scoring table
+			document.getElementById(`frame-${currentFrame}-roll-${currentRoll}`).textContent = hitPinsVisual;
+
+			if (isStrike) {
+				// no second roll after a strike
+				currentRoll++;
+				isStrike = false;
+			}
+			currentRoll++;
+		}
+
+		function handleFrameStatus() {
+			// check if all rolls for this frame are done
+			if (currentRoll > maxRolls) {
+
+				// move to next frame
+				currentFrame++;
+				currentRoll = minRoll;
+				hitPinsInFrame = minPinsPerFrame;
+			}
 		}
 
 		function checkTerminationCondition() {
